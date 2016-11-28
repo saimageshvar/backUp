16cef3656b12e42d4d30eec9cc2c7236c6399ed5
sai <saimageshvar@gmail.com>
2016-11-28 17:38:27 +0530

    events from cms

--- js/index.js
+++ js/index.js
@@ -3,21 +3,39 @@ function myFunction(arr)
 	var icons = ["hi-icon-cog", "hi-icon-clock", "hi-icon-videos", "hi-icon-list", "hi-icon-refresh", "hi-icon-clock", "hi-icon-cog"]
 	
 	//events
-	var outer = document.createElement("div");
-	outer.className = "events";
-	var i;
-	for(i=0;i<7;i++)
+	var domains;
+	for(domains=0; domains < arr[0].events.length; domains++)
 	{
-		var icon = document.createElement("i");
-		icon.className = "hi-icon fa fa-twitter";
-		var child = document.createElement("a");
-		child.href = "#";
-		icon.append(child);
-		outer.append(icon);
+		var outer = document.createElement("div");
+		if(domains != 0)
+		outer.style="display:none";
+		outer.className = "events-"+arr[0].events[domains].name;
+		var i;
+		for(i=0; i<arr[0].events[domains].events.length; i++)
+		{
+			var column = document.createElement("div");
+			column.className = "col-sm-3";
+			var link = document.createElement("a");
+			link.href = "#";
+			link.className = "hi-icon fa fa-twitter";
+			var content = document.createElement("p");
+			content.textContent = arr[0].events[domains].events[i].name;
+			
+			column.append(link);
+			column.append(content);
+			outer.append(column);
+		}
+		document.getElementsByClassName("events")[0].append(outer);
 	}
-	document.getElementsByClassName("domains")[0].append(outer);
 	
+	
+	
+}
 
-	
+function swapDomains(ele)
+{
+	$('.events div').not(".events-"+ele.id).hide();		
+	$(".events-"+ele.id).show();
+	$(".events-"+ele.id+" div").show();			
 	
 }
