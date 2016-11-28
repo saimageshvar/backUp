a604977451ed76feaa186db4644eaaa0541cd1a5
Neeharika <neeha2311@gmail.com>
2016-11-28 17:06:33 +0530

    Add files via upload

--- js/index1.js
+++ js/index1.js
@@ -1,6 +1,6 @@
 function myFunction(arr)
 {
-	var icons = ["hi-icon-cog", "hi-icon-clock", "hi-icon-videos", "hi-icon-list", "hi-icon-refresh", "hi-icon-clock", "hi-icon-cog"]
+	/*var icons = ["fa fa-gear", "fa fa-laptop", "fa fa-terminal", "fa fa-money", "fa fa-android", "fa fa-archive", "fa fa-puzzle-piece"]
 	
 	//events
 	var outer = document.createElement("div");
@@ -8,12 +8,17 @@ function myFunction(arr)
 	var i;
 	for(i=0;i<7;i++)
 	{
-		var icon = document.createElement("i");
-		icon.className = "hi-icon fa fa-twitter";
-		var child = document.createElement("a");
-		child.href = "#";
-		icon.append(child);
-		outer.append(icon);
+		var parent = document.createElement("div");
+		parent.className = "col-sm-2";
+		outer.append(parent);
+		var link = document.createElement("a");
+		link.href = "#set7";
+		link.className = "hi-icon " + icons[i];
+		parent.append(link);
+		var child = document.createElement("p");
+		child.style="";
+		child.textContent=arr[0].categories[i].name;
+		parent.append(child);
 	}
 	document.getElementsByClassName("events-details")[0].append(outer);
 	
@@ -33,8 +38,27 @@ function myFunction(arr)
 		outer.append(child);
 	}
 	document.getElementsByClassName("workshops-details")[0].append(outer);
-	
+	*/
 	//hospitality
+	var outer = document.createElement("div");
 	
+	outer.className = "hospi";
+	var i;	
+	for(i=0;i<6;i++)
+	{
+		var parent = document.createElement("div");
+		parent.className = "tab-pane fade";
+		
+		
+		//parent.id=arr[0].hospitalities[i].title;
+		outer.append(parent);		
+		var child = document.createElement("p")
+		child.append("<html>");
+		child.style="color:#fff"
+		child.textContent=arr[0].hospitalities[i].desc;
+		child.append("</html>");
+		parent.append(child);
+	}
+	document.getElementsByClassName("hospi-details")[0].append(outer);
 	
 }
