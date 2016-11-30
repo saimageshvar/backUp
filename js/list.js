a360dda9c34c72618b6625638d0dd3038de9e3c5
sai <saimageshvar@gmail.com>
2016-11-30 23:10:02 +0530

    fixed form

--- js/list.js
+++ js/list.js
@@ -1,5 +1,6 @@
-function myFunction()
+function populateForm()
 {
+	alert();
 	//degree-names
 	var degrees=[" B.E ","B.A","B.Arch","B.Com","B.Sc","B.Tech","BBA","BCA","DEEE","Diploma","Dual Degree","Intermediate","M.A","M.E","M.Sc","M.Sc(Integrated)","M.Tech","MBA","MCA","MS","Ph.D","Other"]
 	var i;
@@ -14,6 +15,7 @@ function myFunction()
 		outer.append(child);
 	}
 	document.getElementsByClassName("degree")[0].append(outer);
+	$(outer).clone().appendTo($('.degree')[1]);
 	
 	//course-names
 	var courses=["Aeronautical Engineering","Aerospace Engineering","Agricultural & Irrigation Engineering","Aircraft Maintenance Engineering","Animation","Apparel technology","Applied electronics","Applied Mathematics","Architecture","Automobile Engineering","Avionics","Bio Informatics","Bio Medical Engineering","Biotechnology","Ceramic Technology","Charted Accountancy","Chemical Engineering","Chemistry","Civil Engineering","Communication Systems","Computer Science & Engineering","Cryogenic Engineering","Elecrical Engineering","Electrical & Electronics Engineering","Electronic media","Electronics & Communication Engineering","Electronics & Instrumentation","Embedded Systems","Energy Engineering","Engineering Design","Engineering Physics","English Literature","Finance","Fluid Mechanics","Food Technology","Geo Informatics","Harbour Engineering ","High Voltage Engineering","Hospitality Administration","HR","Humanities & Social Sciences","Industrial Engineering","Information & Communications Technology","Information Technology","Internal Combustion Engineering","Logistics","M.Sc. CS-IT","M.Sc. E-Media","Manufacturing Engineering","Marine Engineering","Marketing","Material Science ","Mathematics","Mechanical Engineering","Mechatronics","Media Sciences","Metallurgy","Mining Engineering","Nano Science and Technology","Other","Photonics ","Physics","Printing Technology","Production Engineering","Remote Sensing","Software Engineering","Systems Engineering & Operations Research","Technology Managment","Telecommunication Engineering","Textile Technology","Theoretical Computer Science","Thermal","Transportation Engineering","VLSI Design","Other"]
@@ -28,6 +30,8 @@ function myFunction()
 		outer2.append(child);
 	}
 	document.getElementsByClassName("courses")[0].append(outer2);
+	$(outer2).clone().appendTo($('.courses')[1]);
+
 	
 	//year
 	var year=["1","2","3","4","5"]
@@ -42,4 +46,6 @@ function myFunction()
 		outer3.append(child);
 	}
 	document.getElementsByClassName("year")[0].append(outer3);
+	$(outer3).clone().appendTo($('.year')[1]);
+
 }
