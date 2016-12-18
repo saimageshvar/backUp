9c53ed1c809ab233b370dfacdc38983a56cb8a2e
sai <saimageshvar@gmail.com>
2016-12-18 15:53:24 +0530

    reg validation

--- js/register.js
+++ js/register.js
@@ -1,16 +1,16 @@
-var check;
+var check=false, nameFlag=false, phone=false, pass=false, email=false;
 function validatename(key)
 {
 	var name=key.value;
 	if (!name.match(/^[a-zA-Z. ]*$/))
 	{
 		key.style.borderColor="red";
-		check = false;
+		window.name = false;
 	}
 	else
 	{
 		key.style.borderColor="green";
-		check = true;
+		window.name = true;
 	}
 	key.style.borderWidth="1px";
 }
@@ -18,13 +18,13 @@ function validatephone(key) {
         var phone = key.value;				
         if((phone.match(/^[0-9]+$/) == null) || phone.length != 10)
 		{
-			check = false;
+			window.phone = false;
 			key.style.borderColor="red";
 		}	
 		else
 		{
 			key.style.borderColor="green";
-			check =true;
+			window.phone = true;
 		}
 		key.style.borderWidth="1px";
 } 
@@ -32,13 +32,13 @@ function validatepass(key) {
 		var pass = key.value;
         if(pass.length<8)
 		{
-			check = false;
+			window.pass = false;
 			key.style.borderColor="red";
 		}	
 		else
 		{
 			key.style.borderColor="green";
-			check = true;
+			window.pass = true;
 		}
 		key.style.borderWidth="1px";
 }
@@ -48,23 +48,47 @@ function validatemail(key) {
 		var dotpos = x.lastIndexOf(".");
 		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) 
 		{
-			check = false;
+			window.email = false;
 			key.style.borderColor="red";
 		}
 		else
 		{
 			key.style.borderColor="green";
-			check = true;
+			window.email = true;
 		}			
 		key.style.borderWidth="1px";		
 }
 function Validate(formid)
 {
 	if(check)
+	{		
+		var degree = $('#degree-select');
+        if (degree.val() === '') 
+		{
+			alert("Invalid");
+            //$('#selBooks').focus();
+		}
+
+		/*if($('#degree-select').val()=="")
+		{
+			alert("Invalid")
+		}*/
+		else
+		{
+			formid.method = "post";
+			formid.action = "register.php";
+		}
+	}
+	/*if(check)
 	{
 		formid.method = "post";
 		formid.action = "register.php";
 	}
 	else
-		alert("Invalid")
+		alert("Invalid")*/
+}
+
+function returnCheck()
+{
+	return (window.name && window.phone && window.email && window.pass);
 }
