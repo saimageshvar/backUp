b05bda2c55f2f3622ab1ec4115cfe303df6a913c
sai <saimageshvar@gmail.com>
2016-12-18 15:56:35 +0530

    reg validation

--- js/utils.js
+++ js/utils.js
@@ -94,24 +94,45 @@ $("#login_form").submit(function(e) {
 
 // handle new registration
 $("#registration_form").submit(function(e) { 
-    $.ajax
-    ({ 
-        url: 'register.php',
-        data: $("#registration_form").serialize(),
-        type: 'post',
-        success: function(result)
-        {
-            if(result==1)
+    var flag = returnCheck();
+    if(flag)
+    {
+
+        $.ajax
+        ({ 
+            url: 'register.php',
+            data: $("#registration_form").serialize(),
+            type: 'post',
+            success: function(result)
             {
-                $('.login').modal('toggle');
-                $('#registration_form')[0].reset();
-                BootstrapDialog.show({
-                    title: 'Hey!',
-                    message: 'Registered Successfully. Please login to continue.<br/>Dont forget to check your mail :D',
-                    type: BootstrapDialog.TYPE_SUCCESS,
-                    closable: true,
-                    draggable: true
-                });
+                if(result==1)
+                {
+                    $('#sa_registration_form')[0].reset();
+                    $("#city").empty().trigger('change');
+                    $("#department").empty().trigger('change')
+                    $("#degree").empty().trigger('change')
+                    $("#year").empty().trigger('change')
+                    $("#college").empty().trigger('change')
+
+
+                    BootstrapDialog.show({
+                        title: 'Hey!',
+                        message: 'Registered Successfully. We will contact you soon.<br/>Dont forget to check your mail :D',
+                        type: BootstrapDialog.TYPE_SUCCESS,
+                        closable: true,
+                        draggable: true
+                    });
+                // var li = document.createElement('li');
+                // li.className = "menu-item menu-item-type-post_type menu-item-object-page";
+                // var link = document.createElement('a');
+                // link.href = "logout.php";
+                // label = document.createElement('span');
+                // label.textContent = "Logout";
+                // $(link).append(label);
+                // $(li).append(link);
+                // $('#menu-main-menu').append(li);
+
+
             }
             else if(result==2)
             {
@@ -135,32 +156,60 @@ $("#registration_form").submit(function(e) {
             }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-           alert("some error");
+           BootstrapDialog.show({
+            title: 'Oops!',
+            message: 'Some error occured. Please try later',
+            type: BootstrapDialog.TYPE_DANGER,
+            closable: true,
+            draggable: true
+        });
        }
    });
+    }
+    else
+    {
+        BootstrapDialog.show({
+            title: 'Hey!',
+            message: 'We won\'t spam you. Please enter valid details',
+            type: BootstrapDialog.TYPE_WARNING,
+            closable: true,
+            draggable: true
+        });
+    }
     e.preventDefault();
 });
 
+
 // handle new sa registration
 $("#sa_registration_form").submit(function(e) { 
-    $.ajax
-    ({ 
-        url: 'register.php',
-        data: $("#sa_registration_form").serialize(),
-        type: 'post',
-        success: function(result)
-        {
-            if(result==1)
+    var flag = returnCheck();
+    if(flag)
+    {
+
+        $.ajax
+        ({ 
+            url: 'register.php',
+            data: $("#sa_registration_form").serialize(),
+            type: 'post',
+            success: function(result)
             {
-                $('.registersa').modal('toggle');
-                $('#sa_registration_form')[0].reset();
-                BootstrapDialog.show({
-                    title: 'Hey!',
-                    message: 'Registered Successfully. Please login to continue.<br/>Dont forget to check your mail :D',
-                    type: BootstrapDialog.TYPE_SUCCESS,
-                    closable: true,
-                    draggable: true
-                });
+                if(result==1)
+                {
+                    $('#sa_registration_form')[0].reset();
+                    $("#city").empty().trigger('change');
+                    $("#department").empty().trigger('change')
+                    $("#degree").empty().trigger('change')
+                    $("#year").empty().trigger('change')
+                    $("#college").empty().trigger('change')
+
+
+                    BootstrapDialog.show({
+                        title: 'Hey!',
+                        message: 'Registered Successfully. We will contact you soon.<br/>Dont forget to check your mail :D',
+                        type: BootstrapDialog.TYPE_SUCCESS,
+                        closable: true,
+                        draggable: true
+                    });
                 // var li = document.createElement('li');
                 // li.className = "menu-item menu-item-type-post_type menu-item-object-page";
                 // var link = document.createElement('a');
@@ -173,12 +222,21 @@ $("#sa_registration_form").submit(function(e) {
 
 
             }
-            else if(result==0)
+            else if(result==2)
             {
-                $('.registersa').modal('toggle');
+                BootstrapDialog.show({
+                    title: 'Hey!',
+                    message: 'This email id seems to exist already!',
+                    type: BootstrapDialog.TYPE_WARNING,
+                    closable: true,
+                    draggable: true
+                });
+            }
+            else
+            {
                 BootstrapDialog.show({
                     title: 'Oops!',
-                    message: 'Registration Failed',
+                    message: 'Registration failed!',
                     type: BootstrapDialog.TYPE_DANGER,
                     closable: true,
                     draggable: true
@@ -186,11 +244,29 @@ $("#sa_registration_form").submit(function(e) {
             }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-           alert("some error");
+           BootstrapDialog.show({
+            title: 'Oops!',
+            message: 'Some error occured. Please try later',
+            type: BootstrapDialog.TYPE_DANGER,
+            closable: true,
+            draggable: true
+        });
        }
    });
+    }
+    else
+    {
+        BootstrapDialog.show({
+            title: 'Hey!',
+            message: 'We won\'t spam you. Please enter valid details',
+            type: BootstrapDialog.TYPE_WARNING,
+            closable: true,
+            draggable: true
+        });
+    }
     e.preventDefault();
 });
+
 
 
 // handle sa registration for existing user
